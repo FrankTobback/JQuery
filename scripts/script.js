59f7943135c1cd85ab64ac8e86cedf472c95861d
Frank Tobback <franktobback@gmail.com>
2019-08-08 15:03:33 +0200

    selectors and filters

---
+++ scripts/script.js
@@ -0,0 +1,54 @@
+// SELECTORS
+
+// element selector
+
+// $("h3").css({border: "3px solid blue"});
+
+// class selector
+
+// $(".wrapper").css({border: "3px solid red"});
+
+// ID selector
+
+// $("#clients").css({border: "3px solid yellow"})
+
+
+// FILTERS
+
+// first and last 
+
+// $("header nav li:first").css({border: "3px solid red"});
+// $("header nav li:last").css({border: "3px solid red"});
+
+// first-child and last-child => first and last elements in a section
+
+// $("#contact ul:first-child").css({border: "3px solid red"});
+// $("#contact ul:last-child").css({border: "3px solid red"});
+
+// even
+
+// $("header nav li:even").css({border: "2px solid blue"});
+
+// odd
+
+// $("header nav li:odd").css({border: "2px solid blue"});
+
+// not 
+
+// $("section:not('#contanct')").css({border: "2px solid green"});
+
+// less than
+
+// $("#social-nav li:lt(3)").css({border: "2px solid blue"});
+
+// greater than
+
+// $("#social-nav li:gt(2)").css({border: "2px solid red"});
+
+// attribute filters 
+
+// $("div[class]").css({border: "2px solid pink"});
+
+// attribute with specific value
+
+// $("img[alt=quote]").css({border: "2px solid purple"});
